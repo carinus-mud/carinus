ca5eec5c1aefeaca58c1f84450dd18b1f203aaa7
joeyfogas <joeyfogas@gmail.com>
2018-04-22 15:21:33 +0200

    RESTORE OF FAST SAVE LOC

--- src/save.c
+++ src/save.c
@@ -376,7 +376,7 @@ void fwrite_char( CHAR_DATA * ch, FILE * fp )
    fprintf( fp, "Exp          %d\n", ch->exp );
    fprintf( fp, "Height          %d\n", ch->height );
    fprintf( fp, "Weight          %d\n", ch->weight );
-/*
+
       fprintf(fp,"FastTravelLocs          ");
    for(int i = 0; i < MAX_FAST_TRAVEL_LOCATIONS;i++)
    {
@@ -387,7 +387,7 @@ void fwrite_char( CHAR_DATA * ch, FILE * fp )
 
    }
    fprintf(fp,"\n");
-*/
+
 
 
    if( !xIS_EMPTY( ch->act ) )
@@ -1365,7 +1365,7 @@ void fread_char( CHAR_DATA * ch, FILE * fp, bool preload, bool copyover )
                fMatch = TRUE;
                break;
             }
-/*
+
             if( !strcmp(word, "FastTravelLocs"))
             {
                   int x;
@@ -1380,7 +1380,7 @@ void fread_char( CHAR_DATA * ch, FILE * fp, bool preload, bool copyover )
                
           
             }
-*/
+
             KEY( "Flags", ch->pcdata->flags, fread_number( fp ) );
             KEY( "FPrompt", ch->pcdata->fprompt, fread_string( fp ) );
             break;
